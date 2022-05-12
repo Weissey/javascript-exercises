12ca2f2f6d15f01c964577ed50263b519beb08d9
Weissey <kelvinchangw@gmail.com>
2022-05-11 18:19:45 -0700

    Completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, nextNum) => total + nextNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, nextNum) => total * nextNum);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  let product = 1;
+	if (factorial === 0) {
+    return 1;
+  } else {
+    for (i = factorial; i > 0; i--) {
+      product *= i;
+    }
+  }
+  return product;
 };
 
 // Do not edit below this line
