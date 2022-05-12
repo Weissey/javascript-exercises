cc326400aea83de9c0d12b51d38009807537a3a8
Weissey <kelvinchangw@gmail.com>
2022-05-12 13:01:03 -0700

    Completed exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let processedString = string.toLowerCase().replace(/[^a-z]/g, "")
+    let finalString = processedString.split("").reverse().join("")
+    return processedString == finalString;
 };
 
 // Do not edit below this line
