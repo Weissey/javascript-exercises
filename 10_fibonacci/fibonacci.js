8d92eeef8f38c9143c67aefb45ce5e44ac5c8c34
Weissey <kelvinchangw@gmail.com>
2022-05-12 18:46:11 -0700

    Completed exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let a = 0;
+    let b = 1;
+    if (number < 0) return "OOPS";
+    for (let i = 1; i < number; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+
