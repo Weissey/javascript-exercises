062a274f5c1847df86fde72a745a690be6e7f444
Weissey <kelvinchangw@gmail.com>
2022-05-13 18:21:13 -0700

    Completed exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+// const findTheOldest = function(array) {
+//     const age = array.map(person => (person.yearOfDeath - person.yearOfBirth));
+//     console.log(age);
+//     const sortedAge = age.sort((a, b) => a > b ? 1 : -1);
+//     const sortedLength = sortedAge.length - 1;
+//     const highestAge = sortedAge[sortedLength];
+//     const foundPerson = array.find((item) => {
+//         return item.yearOfDeath - item.yearOfBirth === highestAge;
+//     });
+//     console.log(foundPerson.name);
+//     return foundPerson.name;
+// };
 
-};
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+      return oldestAge < currentAge ? currentPerson : oldest;
+    })
+  };
+
+//oldest 
+
+  function getAge(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+//1970 1942
